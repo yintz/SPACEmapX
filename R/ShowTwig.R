4782652a34d358b711ebbe7451354f56dd92e8a1
yintz <yintz@msn.com>
2023-11-06 13:10:37 +0000

    Update ShowTwig.R

--- R/ShowTwig.R
+++ R/ShowTwig.R
@@ -1,3 +1,13 @@
+# before you run, you must install the SptialinferCNV along with all the relavent package
+# and fully understand what you are doing in order to use this package.
+# this function is designed to select the FFPE samples based on the dendrogram tree's trig.
+# Once you select the the trig, type the trig ID in the function, it will generate the CSV 
+# file with Trig ID for you to load back to the Loupe Brower to view and check.
+# the other function called ShowTrigSectionName stores the section name into CSV list.
+# it is used to locate the section, which more used for further SptialInferCNV to refine.
+# this is the common function to use.
+
+
 ShowTwig <- function(twigID){
   
   Node<- SelectingSubTreeData(my.subtrees, twigID)
