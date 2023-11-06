dbdf692279cb6a22e861f347064d07fec0b39021
yintz <yintz@msn.com>
2023-11-06 14:32:23 +0000

    Update ShowTwig.R

--- R/ShowTwig.R
+++ R/ShowTwig.R
@@ -11,7 +11,6 @@
 ShowTwig <- function(twigID){
   
   Node<- SelectingSubTreeData(my.subtrees, twigID)
-  #Node<- SelectingSubTreeData(my.subtrees, 2404)
   Node$Barcode<-gsub("\\.","-",Node$Barcode)
   Node2<-as.data.frame(str_split_fixed(Node$Barcode, "_", 2))
   Node3<-cbind(Node2,Node$Node)
@@ -24,7 +23,6 @@ ShowTwig <- function(twigID){
     Node5<-subset(Node4,Node4$V1==unique(Node4$V1)[i])
     colnames(Node5)<-c("no","Barcode","Twig")
     Node6<-Node5[,c(2,3)]
-    #write.csv(Node5[,c(2,3)], paste("FFPE_V2_Paitent10_",unique(Node4$V1)[i],".csv", sep = ""))
     write.csv(Node6, paste("FFPE_V2_Paitent10_",unique(Node4$V1)[i],".csv", sep = ""),row.names = FALSE)
   }
 }
