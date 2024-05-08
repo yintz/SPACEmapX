4de4dcf7b782921f83fd3b1ff3a8fb91b287b34c
yintz <yintz@msn.com>
2024-05-08 02:31:16 +0100

    update

--- R/ShowTwig.R
+++ R/ShowTwig.R
@@ -1,7 +1,7 @@
 # before you run, you must install the SptialinferCNV along with all the relavent package
 # and fully understand what you are doing in order to use this package.
 # this function is designed to select the FFPE samples based on the dendrogram tree's trig.
-# Once you select the the trig, type the trig ID in the function, it will generate the CSV 
+# Once you select the the trig, type the trig ID in the function, it will generate the CSV
 # file with Trig ID for you to load back to the Loupe Brower to view and check.
 # the other function called ShowTrigSectionName stores the section name into CSV list.
 # it is used to locate the section, which more used for further SptialInferCNV to refine.
@@ -9,20 +9,20 @@
 
 
 ShowTwig <- function(twigID){
-  
+
   Node<- SelectingSubTreeData(my.subtrees, twigID)
   Node$Barcode<-gsub("\\.","-",Node$Barcode)
   Node2<-as.data.frame(str_split_fixed(Node$Barcode, "_", 2))
   Node3<-cbind(Node2,Node$Node)
   Node4<-Node3
   length(unique(Node4$V1))
-  
+
   for (i in c(1:length(unique(Node4$V1)))){
     print(i)
     print(unique(Node4$V1)[i])
     Node5<-subset(Node4,Node4$V1==unique(Node4$V1)[i])
     colnames(Node5)<-c("no","Barcode","Twig")
     Node6<-Node5[,c(2,3)]
-    write.csv(Node6, paste("FFPE_V2_Paitent10_",unique(Node4$V1)[i],".csv", sep = ""),row.names = FALSE)
+    write.csv(Node6, paste("FFPE_V2_Paitent_",unique(Node4$V1)[i],".csv", sep = ""),row.names = FALSE)
   }
 }
--- R/ShowTwigSectionName.R
+++ R/ShowTwigSectionName.R
@@ -1,7 +1,7 @@
 # before you run, you must install the SptialinferCNV along with all the relavent package
 # and fully understand what you are doing in order to use this package.
 # this function is designed to select the FFPE samples based on the dendrogram tree's trig.
-# Once you select the the trig, type the trig ID in the function, it will generate the CSV 
+# Once you select the the trig, type the trig ID in the function, it will generate the CSV
 # file with Section ID for you to load on inferCNV to replot.
 # the other function called ShowTrig stores the Trig name into CSV list.
 # it is used to load back on loupe browser to refine the selection.
@@ -9,14 +9,14 @@
 
 
 ShowTwigSectionName <- function(twigID){
-  
+
   Node<- SelectingSubTreeData(my.subtrees, twigID)
   Node$Barcode<-gsub("\\.","-",Node$Barcode)
   Node2<-as.data.frame(str_split_fixed(Node$Barcode, "_", 2))
   Node3<-cbind(Node2,Node$Node)
   Node4<-Node3
   length(unique(Node4$V1))
-  
+
   for (i in c(1:length(unique(Node4$V1)))){
     print(i)
     print(unique(Node4$V1)[i])
@@ -24,6 +24,6 @@ ShowTwigSectionName <- function(twigID){
     colnames(Node5)<-c("no","Barcode","Twig")
     Node5[,3]<-unique(Node4$V1)[i]
     Node6<-Node5[,c(2,3)]
-    write.csv(Node6, paste("FFPE_V2_Paitent10_",unique(Node4$V1)[i],".csv", sep = ""),row.names = FALSE)
+    write.csv(Node6, paste("FFPE_V2_Paitent_",unique(Node4$V1)[i],".csv", sep = ""),row.names = FALSE)
   }
 }
