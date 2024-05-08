29c4e577629b53520ed1496687e71df12032aa74
yintz <yintz@msn.com>
2024-05-08 03:30:48 +0100

    Create ImportHistologicalAnnotations.R

---
+++ R/ImportHistologicalAnnotations.R
@@ -0,0 +1,19 @@
+#' Importing histological annotations of Visium barcodes and appending a section name to the barcodes. 
+#' 
+#' the LoupeBrower.
+#' ImportHistologicalAnnotations()
+#' 
+#' @param SectionName A character string for section name.
+#' @param InputAnnotationFile A file path to a .csv file, with annotations (for example, output from LoupeBrowser after manual annotations)
+#' @return A dataframe of barcodes with appended section names
+#' @examples
+#' ImportHistologicalAnnotations("H1_2", "./H1_2_Final_Consensus_Annotations.csv")
+
+ImportHistologicalAnnotations <- function(SectionName, InputAnnotationFile) {
+  input <- read.csv(paste0(InputAnnotationFile))
+  names(input)[2] <- "Histology"
+  input <- input %>%
+    mutate(Barcode = str_replace_all(Barcode, "-", "."))
+  input$Barcode <- paste0(SectionName, "_", input$Barcode)
+  return(input)
+}
