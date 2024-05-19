0aabc17c478680c0ee831df71b73c36393b5dfad
yintz <yintz@msn.com>
2024-05-19 14:56:53 +0100

    Update require_above_min_mean_expr_cutoff.R

--- R/require_above_min_mean_expr_cutoff.R
+++ R/require_above_min_mean_expr_cutoff.R
@@ -1,3 +1,23 @@
+
+#' @title require_above_min_mean_expr_cutoff ()
+#'
+#' @description Filters out genes that have fewer than the corresponding mean value across all cell values.
+#'
+#' @param infercnv_obj  infercnv_object
+#'
+#' @param min_mean_expr_cutoff  the minimum mean value allowed for a gene to be retained in the expression matrix.
+#'
+#' @return infercnv_obj  the infercnv_object with lowly or unexpressed genes removed.
+#'
+#' @keywords internal
+#' @noRd
+#'
+
+
+
+
+
+
 require_above_min_mean_expr_cutoff <- function(infercnv_obj, min_mean_expr_cutoff) {
     
     flog.info(paste("::above_min_mean_expr_cutoff:Start", sep=""))
