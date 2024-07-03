86b4fd57b71f13beb0648db6b3039b78191a1065
yintz <yintz@msn.com>
2024-07-03 01:05:43 +0100

    Create Dendrogram.R

---
+++ R/Dendrogram.R
@@ -0,0 +1,15 @@
+
+
+
+
+Dendrogram <- function(location){
+
+clustering <- read.dendrogram(file=location)
+clustering_phylo <- as.phylo(clustering)
+# Visualizing Tree
+#Next, we use the dendrogram file to visualize the dendrogram itself.
+my.subtrees = subtrees(clustering_phylo)  # subtrees() to subset
+png("Dendrogram.png",width=10000,height=10500, res = 300)
+plot(clustering_phylo,show.tip.label = FALSE)
+nodelabels(text=1:clustering_phylo$Nnode,node=1:clustering_phylo$Nnode+Ntip(clustering_phylo))
+dev.off()
